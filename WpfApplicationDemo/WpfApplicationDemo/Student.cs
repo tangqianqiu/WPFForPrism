866904d67871afd5601bc4900b48ee3fd844a4c2
tangqianqiu <584432285@qq.com>
2018-07-20 21:51:51 +0800

    上传本地wpf for prism的小程序

---
+++ WpfApplicationDemo/WpfApplicationDemo/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplicationDemo
+{
+    public class Student:INotifyPropertyChanged
+    {
+        private string _firstName = "";
+        private string _secondName = "";
+
+        public Student(string firstName, string secondName)
+        {
+            this._firstName = firstName;
+            this._secondName = secondName;
+        }
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+      
+        public string SecondName
+        {
+            get { return _secondName; }
+            set { _secondName = value; }
+        }
+
+        void Notify(string propName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
